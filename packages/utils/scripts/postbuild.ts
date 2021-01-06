2ce14a428c3bc23d518a9897788f7604c57a79b0
YashTotale <totaleyash@gmail.com>
2021-01-06 12:36:09 -0800

    Work on utils package

--- packages/utils/scripts/postbuild.ts
+++ packages/utils/scripts/postbuild.ts
@@ -1,31 +1,27 @@
 import { join } from "path";
-import { writeFile } from "fs/promises";
+import { writeFile, rm } from "fs/promises";
+import recursive from "recursive-readdir";
 
-const cjs = async () => {
+const postbuild = async () => {
   try {
-    await writeFile(
+    writeFile(
       join("cjs", "index.js"),
       "'use strict';\n\nif (process.env.NODE_ENV === 'production') {\n  module.exports = require('./hurl-utils.min.js');" +
         "\n} else {\n  module.exports = require('./hurl-utils.js');\n}"
     );
+    recursive("cjs").then((files) => {
+      files.forEach((file) => {
+        if (file.length > 5) {
+          const ending = file.substr(file.length - 5);
+          if (ending === ".d.ts" && file !== "cjs/index.d.ts") {
+            rm(file);
+          }
+        }
+      });
+    });
   } catch (e) {
     console.log(e);
     process.exit(1);
-  }
-};
-
-const postbuild = async () => {
-  switch (process.env.BUILD_ENV) {
-    case "cjs": {
-      await cjs();
-      break;
-    }
-    case "umd": {
-      break;
-    }
-    default: {
-      cjs();
-    }
   }
 };
 
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,1 +1,29 @@
-export const test = () => console.log("yay works");
+// Node
+import { readdir } from "fs/promises";
+import { join } from "path";
+
+// Internals
+import logger from "./logger";
+
+export class HurlUtils {
+  private dir: string;
+
+  constructor(dir?: string) {
+    this.dir = dir ?? process.cwd();
+  }
+
+  changeDir(dir: string) {
+    this.dir = dir;
+  }
+
+  getHurlDir(): Promise<string[]> {
+    return readdir(join(this.dir, ".hurl"));
+  }
+
+  getTemplates(): Promise<string[]> {
+    return readdir(join(this.dir, ".hurl", "templates"));
+  }
+}
+
+process.on("uncaughtException", (e: Error) => logger.error(e));
+process.on("unhandledRejection", (e: Error) => logger.error(e));
