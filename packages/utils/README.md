df106ef5624763060d82a6180c2a754b443a289f
YashTotale <totaleyash@gmail.com>
2021-01-06 11:17:53 -0800

    Added utils package

---
+++ packages/utils/README.md
@@ -0,0 +1,7 @@
+<p align="center">
+<img src="https://raw.githubusercontent.com/hurl-org/hurl/main/static/logo-dark.png" width="300">
+<p>
+
+<h2 align="center">Hurl: <small>Template File Generator</small></h2>
+
+Work in progress
---
+++ packages/utils/rollup.config.js
@@ -0,0 +1,88 @@
+import typescript from "@rollup/plugin-typescript";
+import replace from "@rollup/plugin-replace";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+
+const name = "hurl-utils";
+
+const cjs = [
+  {
+    input: "src/index.ts",
+    output: {
+      dir: "cjs",
+      format: "cjs",
+      sourcemap: true,
+      esModule: false,
+      entryFileNames: `${name}.js`,
+    },
+    plugins: [
+      nodeResolve(),
+      typescript({ declaration: true, declarationDir: "cjs" }),
+      replace({ "process.env.NODE_ENV": JSON.stringify("development") }),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: `cjs/${name}.min.js`,
+      format: "cjs",
+      sourcemap: true,
+    },
+    plugins: [
+      nodeResolve(),
+      typescript(),
+      replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
+      terser(),
+    ],
+  },
+];
+
+const umd = [
+  {
+    input: "src/index.ts",
+    output: {
+      file: `umd/${name}.js`,
+      format: "umd",
+      name: "HurlUtils",
+    },
+    plugins: [
+      nodeResolve(),
+      typescript({ sourceMap: false }),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("development"),
+      }),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: `umd/${name}.min.js`,
+      format: "umd",
+      name: "HurlUtils",
+    },
+    plugins: [
+      typescript({ sourceMap: false }),
+      nodeResolve(),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+      }),
+      terser(),
+    ],
+  },
+];
+
+const createConfig = () => {
+  switch (process.env.BUILD_ENV) {
+    case "cjs": {
+      return [...cjs];
+    }
+    case "umd": {
+      return [...umd];
+    }
+    default: {
+      return [...cjs, ...umd];
+    }
+  }
+};
+
+export default createConfig();
